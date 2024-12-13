b6de77ded513ae700c2b15dcc3f795d8f339a6b3
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-12-13 12:11:02 +0100

    Completeing Context-Lab

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,3 +1,29 @@
-export const ThemeProvider = ({ children }) => children
+import { createContext, useContext, useState } from "react"
 
-export const useTheme = () => ({ theme: "light" })
+const ThemeContext = createContext()
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light")
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme: () =>
+          setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light")),
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext)
+
+  if (!context) {
+    throw new Error("we have no context")
+  }
+
+  return context
+}
